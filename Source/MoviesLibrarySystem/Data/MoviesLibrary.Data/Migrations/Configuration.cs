88477f18925851f67381644e0236dffb438b0ad8
toshko <toshkooooooooooo@gmail.com>
2016-03-04 17:30:58 +0200

    create admin area

--- Source/MoviesLibrarySystem/Data/MoviesLibrary.Data/Migrations/Configuration.cs
+++ Source/MoviesLibrarySystem/Data/MoviesLibrary.Data/Migrations/Configuration.cs
@@ -17,30 +17,6 @@ namespace MoviesLibrary.Data.Migrations
 
         protected override void Seed(MoviesLibraryDbContext context)
         {
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-
-            string roleName = "Admin";
-            IdentityResult roleResult;
-
-            if (!roleManager.RoleExists(roleName))
-            {
-                PasswordHasher hasher = new PasswordHasher();
-                var userManager = new UserManager<User>(new UserStore<User>(context));
-
-                roleResult = roleManager.Create(new IdentityRole(roleName));
-                var admin = new User
-                {
-                    Email = "TodorDimitrov@gmail.com",
-                    PasswordHash = hasher.HashPassword("todor.OneMoment1984"),
-                    UserName = "TodorDimitrov@gmail.com"
-                };
-
-                context.Users.Add(admin);
-                context.SaveChanges();
-                userManager.UpdateSecurityStamp(admin.Id);
-
-                userManager.AddToRole(admin.Id, roleName);
-            }
         }
     }
 }
